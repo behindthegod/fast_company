74b437132cc39a42b26571a769ded885b3c0053f
behindthegod <divizionby0@gmail.com>
2022-07-27 19:01:13 +0300

    --

---
+++ src/components/GroupList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GroupList = ({items, valueProperty, contentProperty}) => {
+    return (
+            <ul className="list-group">
+                {Object.keys(items).map(item => <li key={items[item][valueProperty]} className="list-group-item">{items[item][contentProperty]}</li>)}
+            </ul>
+    );
+};
+GroupList.defaultProps = {
+    valueProperty:"_id",
+    contentProperty:"name"
+}
+GroupList.propTypes = {
+    items:PropTypes.object.isRequired,
+    valueProperty:PropTypes.string.isRequired,
+    contentProperty:PropTypes.string.isRequired
+}
+
+export default GroupList;
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,32 +1,49 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import User from "./User";
 import Pagination from "./Pagination";
 import PropTypes from "prop-types";
+import GroupList from "./GroupList";
+import api from "../api";
 
-const Users = ({ users, ...rest }) => {
+const Users = ({users, ...rest}) => {
     const count = users.length;
     const pageSize = 4;
     const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfessions] = useState();
+
+    useEffect(() => {
+        api.professions.fetchAll().then((data) => setProfessions(data));
+    }, [])
+
+
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
     };
+    const handleProfessionSelect = (params) => {
+        console.log(params);
+    };
+
     return (
         <>
+            {professions && <GroupList
+                items={professions}
+                onItemSelect={handleProfessionSelect}
+            />}
             <table>
                 <thead>
-                    <tr>
-                        <th>Имя</th>
-                        <th>Качества</th>
-                        <th>Профессия</th>
-                        <th>Встретился, раз</th>
-                        <th>Оценка</th>
-                        <th>Избранное</th>
-                    </tr>
+                <tr>
+                    <th>Имя</th>
+                    <th>Качества</th>
+                    <th>Профессия</th>
+                    <th>Встретился, раз</th>
+                    <th>Оценка</th>
+                    <th>Избранное</th>
+                </tr>
                 </thead>
                 <tbody>
-                    {users.map((user) => (
-                        <User key={user._id} {...user} {...rest} />
-                    ))}
+                {users.map((user) => (
+                    <User key={user._id} {...user} {...rest} />
+                ))}
                 </tbody>
             </table>
 
@@ -38,9 +55,11 @@ const Users = ({ users, ...rest }) => {
             />
         </>
     );
-};
+}
+;
 
-Users.propTypes = {
+Users.propTypes =
+{
     users: PropTypes.array.isRequired
 };
 
